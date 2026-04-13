801c07aa75fe54f6d687a79ef15fc0a74de3940c
kokchun <kokchungiang@gmail.com>
2026-04-13 13:06:20 +0200

    streamlit init

--- 06_pydanticai_fastapi_chatbot/frontend.py
+++ 06_pydanticai_fastapi_chatbot/frontend.py
@@ -1,8 +1,11 @@
-import streamlit as st 
+import streamlit as st
 
 
 def layout():
     st.markdown("# Chat with Ro Båt")
+    st.markdown(
+        "RO BÅT is a funny robot that can help you out with programming tasks. However he doesn't directly answer your question, usually he asks another question back."
+    )
 
 
 if __name__ == "__main__":
