11aa070ac9ce9a5eb988060c70b623d5f0002f9a
kokchun <kokchungiang@gmail.com>
2026-04-13 13:13:14 +0200

    initializing session state

--- 06_pydanticai_fastapi_chatbot/frontend.py
+++ 06_pydanticai_fastapi_chatbot/frontend.py
@@ -1,11 +1,24 @@
 import streamlit as st
 
 
+# save message_history into session state
+# want to save messages into session state
+
+def init_session_state():
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+
+# so that we can loop through them and display them in the frontend
+# send in users question to API
+# display bot answer
+
 def layout():
     st.markdown("# Chat with Ro Båt")
     st.markdown(
         "RO BÅT is a funny robot that can help you out with programming tasks. However he doesn't directly answer your question, usually he asks another question back."
     )
+    st.write(st.session_state)
 
 
 if __name__ == "__main__":
